07666604ff2a3203b3ec06b6a08c74660b76d2fd
Katelyn-Collier <katelyncollier14@gmail.com>
2023-04-03 15:30:52 -0500

    Exercise 1

---
+++ IDepartmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLandORM
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDepartments();
+
+        void InsertDepartment(string newDepartmentName);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace SQLandORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Configuration
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            #endregion
+
+            IDbConnection conn = new MySqlConnection(connString);
+            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
+
+            Console.WriteLine("Hello! Please press 'Enter' to view the current departments:");
+            Console.ReadLine();
+
+            var depos = repo.GetAllDepartments();
+            Print(depos);
+
+
+            Console.WriteLine("Would you like to add a department?");
+            string userResponse = Console.ReadLine();
+
+            if (userResponse.ToLower() == "yes")
+            {
+                Console.WriteLine("What would you like to name this new department?");
+                userResponse = Console.ReadLine();
+
+                repo.InsertDepartment(userResponse);
+                Print(repo.GetAllDepartments());
+            }
+
+            Console.WriteLine("Goodbye!");
+        }
+
+        private static void Print(IEnumerable<Department> depos)
+        {
+            foreach (var depo in depos)
+            {
+                Console.WriteLine($"Id: {depo.DepartmentID} // Name: {depo.Name}");
+            }
+        }
+    }
+}
